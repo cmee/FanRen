3ccc53412bf4c08dd2e9f0fb0dfdea62d2d8d5c9
rtsbtx <rtsczq1986@gmail.com>
2022-07-23 17:47:41 +0800

    添加了一个符文阵法的特效

--- Assets/Script/SceneInit/QingNiuTownInitScript.cs
+++ Assets/Script/SceneInit/QingNiuTownInitScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//剧本脚本
 public class QingNiuTownInitScript : MonoBehaviour
 {
 
@@ -9,52 +10,48 @@ public class QingNiuTownInitScript : MonoBehaviour
     public GameObject[] wayPaths;
     public GameObject carBGM;
     //public GameObject carStopSound;
+    private int nextIndex = 0;
+    private bool flag = true;
 
     // Start is called before the first frame update
     void Start()
     {
     }
 
-
-    int nextIndex = 0;
-    bool flag = true;
-
     // Update is called once per frame
     void Update()
     {
-        //horseCar.transform.LookAt(wayPaths[0].transform);
-        //horseCar.transform.Translate(Vector3.forward * Time.deltaTime * 1.5f);
-
         if (nextIndex > wayPaths.Length - 1)
         {
-            //Destroy(this.gameObject);
-            
             if (flag)
             {
                 flag = false;
                 MyAudioManager.GetInstance().PlaySE("SoundEff/horse_stop2");
                 carBGM.SetActive(false);
+                Invoke("OnHorseCarStop", 1); //延迟1秒是为了等马叫声停止才下车，显得真实
             }
             return;
         }
-        //horseCar.transform.LookAt(wayPaths[nextIndex].transform);
 
-        Vector3 aa = horseCar.transform.forward;
-        aa.y = 0f;
+        Vector3 horseCarForward = horseCar.transform.forward;
+        horseCarForward.y = 0f;
 
-        Vector3 bb = (wayPaths[nextIndex].transform.position - horseCar.transform.position).normalized;
-        bb.y = 0f;
+        Vector3 horseCarTowardTargetPoint = (wayPaths[nextIndex].transform.position - horseCar.transform.position).normalized;
+        horseCarTowardTargetPoint.y = 0f;
 
-        Vector3 a = Vector3.RotateTowards(aa, bb, 0.7f * Time.deltaTime, 0f);
-        horseCar.transform.rotation = Quaternion.LookRotation(a);
+        Vector3 horseRotateToward = Vector3.RotateTowards(horseCarForward, horseCarTowardTargetPoint, 0.7f * Time.deltaTime, 0f);
+        horseCar.transform.rotation = Quaternion.LookRotation(horseRotateToward);
 
-        
-
-        horseCar.transform.Translate(bb * Time.deltaTime * 5f, Space.World);
+        horseCar.transform.Translate(horseCarTowardTargetPoint * Time.deltaTime * 5f, Space.World);
         if (Vector3.Distance(horseCar.transform.position, wayPaths[nextIndex].transform.position) <= 0.2f)
         {
             nextIndex++;
         }
+    }
+
+    private void OnHorseCarStop()
+    {
+        //马车停止后，韩立和三叔下车和一些老朋友调侃对话
     }
 
 }
